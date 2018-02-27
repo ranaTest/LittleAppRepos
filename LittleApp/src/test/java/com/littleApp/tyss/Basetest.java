af5b244a5ec3e91c0f717c272b88f17bb9c9f5db
tyss <tyss@localhost>
2018-02-27 15:13:16 +0530

    modified base class

--- LittleApp/src/test/java/com/littleApp/tyss/Basetest.java
+++ LittleApp/src/test/java/com/littleApp/tyss/Basetest.java
@@ -2,10 +2,7 @@ package com.littleApp.tyss;
 
 public class Basetest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("hi i am good");
-		//i am not bad
-	}
+
+	
 
 }
